46caa1e5d047ce33da63ffc913ebf0827eb76561
Nir Moav <nirm@wix.com>
2015-03-13 16:12:31 +0200

    connecting bar to work with todo list

--- scripts/todo/TodoBar.js
+++ scripts/todo/TodoBar.js
@@ -1,6 +1,18 @@
-define(['react', 'todo/TodoBar.rt'], function(React, template) {
+define(['react', 'dispatcher/dispatcher', 'actions/TodoActions',
+	'todo/TodoBar.rt'], function(React, dispatcher, TodoActions, template) {
 	return React.createClass({
 		displayName: 'TodoBar',
+		mixins: [React.addons.LinkedStateMixin],
+		getInitialState: function() {
+			return {
+				todoTask: 'say hello'
+			};
+		},
+		addTodo: function() {
+			var action = TodoActions.createAddAction(this.state.todoTask);
+			dispatcher.dispatch(action);
+			this.setState({todoTask: null});
+		},
 		render: template
 	});
 });
--- scripts/todo/TodoBar.rt.js
+++ scripts/todo/TodoBar.rt.js
@@ -4,6 +4,9 @@ define([
 ], function (React, _) {
     'use strict';
     return function () {
-        return React.createElement('div', {}, React.createElement('label', {}, '\n\t\tAdd your Todo task\n\t\t', React.createElement('input', { 'type': 'text' }), React.createElement('button', {}, 'Add')));
+        return React.createElement('div', {}, React.createElement('label', {}, '\n\t\tAdd your Todo task\n\t\t', React.createElement('input', {
+            'type': 'text',
+            'valueLink': this.linkState('todoTask')
+        }), React.createElement('button', { 'onClick': this.addTodo }, 'Add')));
     };
 });
